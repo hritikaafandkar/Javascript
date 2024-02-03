70f1dc16e01160b4386af21335f92bbe730127e0
[hritikaafandkar] <[hritikaafandkar@gmail.com]>
2024-02-03 13:58:37 +0530

    First commit

---
+++ Index.js
@@ -0,0 +1,16 @@
+//Prints second value (hello)
+var  a = "Hello World";
+var a ="Hello";
+console.log(a);
+
+//Prints both the value (Hritika & Gayatri)
+let name ="Hritika";
+console.log(name);
+name="Gayatri";
+console.log(name);
+
+//prints only one value (18) and for second variable gives Typeerror:Assignment to constant variable.
+const age = "18";
+console.log(age);
+age = "20";
+console.log(age);
