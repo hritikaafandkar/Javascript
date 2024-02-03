ac24a9ef3ad0dcf2bffe86acbcf9b8a0602b944b
[hritikaafandkar] <[hritikaafandkar@gmail.com]>
2024-02-03 14:23:48 +0530

    basic

--- Index.js
+++ Index.js
@@ -1,4 +1,4 @@
-//Prints second value (hello)
+/*//Prints second value (hello)
 var  a = "Hello World";
 var a ="Hello";
 console.log(a);
@@ -13,4 +13,17 @@ console.log(name);
 const age = "18";
 console.log(age);
 age = "20";
-console.log(age);
+console.log(age);*/
+
+
+/*var var = "Hello "; //SyntaxError: Unexpected token 'var'
+console.log(var);
+let let ="ABC"; //SyntaxError: let is disallowed as a lexically bound name
+console.log(let);
+const const ="XYZ"; //SyntaxError: Unexpected token 'const'
+console.log(const);*/
+
+var name ="Hritika";
+typeof(name);
+//var name=123;
+console.log(typeof(name));
