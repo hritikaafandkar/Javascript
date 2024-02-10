14a01393236eca06f341f292906bd3496751607a
[hritikaafandkar] <[hritikaafandkar@gmail.com]>
2024-02-10 11:36:18 +0530

    Datatypes in Javascript

---
+++ Datatypes.js
@@ -0,0 +1,18 @@
+var student={
+    name : "Hritika",
+    age : 22,
+    skills:["HTML, CSS, Javascript"]
+};
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.skills);
+console.log("--------------------------------------");
+var student2=student
+student2.name="Divya"
+console.log(student2.name);
+console.log("--------------------------------------");
+var str ="hello world";
+console.log(str);
+var str2=str.toUpperCase();
+console.log(str2);
