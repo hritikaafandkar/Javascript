56b4a7a999cc50696254edc4f643e4afa61e5248
[hritikaafandkar] <[hritikaafandkar@gmail.com]>
2024-02-11 10:12:06 +0530

    Srting Search Methods

---
+++ Strings.js
@@ -0,0 +1,23 @@
+// let name="Hii My name is ABC";
+// console.log(name);
+// let length = name.length;
+// console.log(length);
+
+// var b ='123';
+// console.log(typeof(b)); 
+// //var name=123;
+// console.log(typeof(Number(b)));
+
+//11-02-2024
+//String Search Methods
+
+let text = "Please locate where 'locate' occurs!";
+let index = text.indexOf("locate");
+console.log(index);
+
+let last=text.lastIndexOf("locate");
+console.log(last);
+
+
+let search=text.search("locate");
+console.log(search);
